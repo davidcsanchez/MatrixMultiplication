c8846cba760385f8762eb8117e059bf3e587408d
Davoestacogido <90717947+Davoestacogido@users.noreply.github.com>
2022-10-21 22:07:34 +0100

    deserializer tested

---
+++ src/main/java/es/ulpgc/deserializers/MtxToSparseDeserializer.java
@@ -0,0 +1,56 @@
+package es.ulpgc.deserializers;
+
+import es.ulpgc.MatrixDeserializer;
+import es.ulpgc.matrices.CcsMatrix;
+import es.ulpgc.matrices.Coordinate;
+import es.ulpgc.matrices.SparseMatrix;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MtxToSparseDeserializer implements MatrixDeserializer {
+
+    private final List<Coordinate> items = new ArrayList<>();
+    private int size;
+
+    @Override
+    public SparseMatrix deserialize(String filename) {
+        InputStream is = MtxToSparseDeserializer.getInputStream(filename);
+        BufferedReader reader = MtxToSparseDeserializer.getBufferedReader(is);
+        return readFile(reader);
+    }
+
+    private static BufferedReader getBufferedReader(InputStream is) {
+        InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
+        return new BufferedReader(streamReader);
+    }
+
+    private static InputStream getInputStream(String filename) {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        return classloader.getResourceAsStream(filename);
+    }
+
+    private SparseMatrix readFile(BufferedReader reader) {
+        size = reader.lines().filter(line -> !line.startsWith("%"))
+            .map(this::getSize).findFirst().orElse(0);
+        reader.lines().forEach(line -> saveValues(getLineValues(line)));
+        return new SparseMatrix(items, size);
+    }
+
+    private int getSize(String line) {
+        return Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray()[0];
+    }
+
+    private void saveValues(double[] lineValues) {
+        this.items.add(new Coordinate((int) lineValues[0] - 1, (int) lineValues[1] - 1, lineValues[2]));
+    }
+
+    private double[] getLineValues(String line) {
+        return Arrays.stream(line.split(" ")).mapToDouble(Double::parseDouble).toArray();
+    }
+}
--- src/test/java/DeserializerTest.java
+++ src/test/java/DeserializerTest.java
@@ -1,17 +1,44 @@
+import es.ulpgc.Matrix;
+import es.ulpgc.MatrixBuilder;
+import es.ulpgc.MatrixDeserializer;
+import es.ulpgc.builders.DenseMatrixBuilder;
+import es.ulpgc.builders.SparseMatrixBuilder;
 import es.ulpgc.deserializers.MtxToCssDeserializer;
+import es.ulpgc.deserializers.MtxToSparseDeserializer;
 import es.ulpgc.matrices.CcsMatrix;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+
+@RunWith(Parameterized.class)
 public class DeserializerTest {
+
+    private final MatrixDeserializer deserializer;
+
+    public DeserializerTest(MatrixDeserializer deserializer) {
+        this.deserializer = deserializer;
+    }
+
     @Test
-    public void get_the_matrix() {
-        MtxToCssDeserializer deserializer = new MtxToCssDeserializer();
-        CcsMatrix matrix = deserializer.deserialize("mc2depi - copia.mtx");
+    public void test_matrix_values() {
+        Matrix matrix = deserializer.deserialize("mc2depi - copia.mtx");
         assertThat(matrix.value(0,0)).isEqualTo(5d);
         assertThat(matrix.value(3, 2)).isEqualTo(9d);
         assertThat(matrix.value(1, 1)).isEqualTo(8d);
         assertThat(matrix.value(2, 2)).isEqualTo(3d);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<MatrixDeserializer> implementations() {
+        return List.of(
+            new MtxToCssDeserializer(),
+            new MtxToSparseDeserializer()
+        );
     }
 }
